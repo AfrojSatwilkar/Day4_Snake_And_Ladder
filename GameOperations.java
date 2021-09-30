1c793e89dae6f03b375d31f5705b1aa2a745bd66
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-30 20:34:22 +0530

    Commit Start Position

--- GameOperations.java
+++ GameOperations.java
@@ -1,5 +1,6 @@
 package com.company1.SnakeAndLadder;
 
 public class GameOperations {
+    public static int START_POSITION ,PREVIOUS_POSITION ,COUNT=0;
 
 }
--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -4,5 +4,6 @@ package com.company1.SnakeAndLadder;
 public class SnakeAndLadderGame {
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game");
+        GameOperations.START_POSITION =0;
     }
 }
