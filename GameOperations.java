fa0ee2c5765238cba4f57950277f7138a11c7ee7
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-30 21:40:15 +0530

    Commit next move

--- GameOperations.java
+++ GameOperations.java
@@ -2,11 +2,33 @@ package com.company1.SnakeAndLadder;
 import java.util.Random;
 
 public class GameOperations {
-    public static int START_POSITION ,PREVIOUS_POSITION ,COUNT=0;
-
-    public static void roll(){
+    public static int START_POSITION, PREVIOUS_POSITION, DICE_NUMBER, COUNT = 0;
+    Random num =new Random();
+    public void roll() {
         PREVIOUS_POSITION = START_POSITION;
-        Random num = new Random();
-        START_POSITION = num.nextInt(5)+1;
+        DICE_NUMBER = num.nextInt(5) + 1;
+        COUNT++;
+        option();
+    }
+    public void option(){
+        int Option = num.nextInt(2) + 1;
+        nextMove(Option);
+    }
+    public void nextMove(int Option){
+        switch (Option){
+            case 1 ->
+                    //no play
+                    System.out.println("No play "+START_POSITION+" Location");
+            case 2 -> {
+                //ladder
+                START_POSITION += DICE_NUMBER;
+                System.out.println("Next position :" + START_POSITION);
+            }
+            case 3 -> {
+                //snake
+                START_POSITION -= DICE_NUMBER;
+                System.out.println("Position :" + START_POSITION);
+            }
+        }
     }
 }
--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -7,6 +7,7 @@ public class SnakeAndLadderGame {
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game");
         GameOperations.START_POSITION =0;
-        GameOperations.roll();
+        GameOperations GO =new GameOperations();
+        GO.roll();
     }
 }
