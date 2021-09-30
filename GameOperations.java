a6298417d7b33ec00a3234772b05f79e27db89f1
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-09-30 21:00:05 +0530

    Get a number between 1 to 6

--- GameOperations.java
+++ GameOperations.java
@@ -1,6 +1,12 @@
 package com.company1.SnakeAndLadder;
+import java.util.Random;
 
 public class GameOperations {
     public static int START_POSITION ,PREVIOUS_POSITION ,COUNT=0;
 
+    public static void roll(){
+        PREVIOUS_POSITION = START_POSITION;
+        Random num = new Random();
+        START_POSITION = num.nextInt(5)+1;
+    }
 }
--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,9 +1,12 @@
 package com.company1.SnakeAndLadder;
 
 
+import java.util.Random;
+
 public class SnakeAndLadderGame {
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game");
         GameOperations.START_POSITION =0;
+        GameOperations.roll();
     }
 }
